ede9f18d18ecdaa3cdb09dc8b98f91c8fcee45a2
Ben <benjamin.fulton99@gmail.com>
2021-05-24 18:07:37 -0600

    Updates verified sender

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "/api/v1/email", {"to": "maj3st1@gmail.com",
-                                                  "from": "user_instructor@email.com",
+response = requests.put(BASE + "/api/v1/email", {"to": "turingmeshtest@gmail.com",
+                                                  "from": "turingmesh@email.com",
                                                   "subject": "Feedback",
                                                   "content": "Your project Feedback is available at www.google.com"
                                                   })
